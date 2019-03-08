228def427c03e62009399c817efbfb37f488b4dc
samuel-cavalcanti <scavalcanti111@gmail.com>
2019-03-08 09:02:03 -0300

    Cart-Pole funcionando

--- CartPole-v1/solveCartPoleV1.py
+++ CartPole-v1/solveCartPoleV1.py
@@ -10,17 +10,18 @@ from keras import models
 from keras.optimizers import Adam
 
 
-LEARNING_RATE = 0.001
+
 
 def toInputArray(state):
     return np.reshape(state,(1,-1))
 
 
 def buildModel(input_size, output_size):
+        LEARNING_RATE = 0.001
                 
         input_layer = layers.Input(shape=(input_size,))
 
-        first_hidden_layer = layers.Dense(24, activation="relu")(input_layer)
+        first_hidden_layer = layers.Dense(48, activation="relu")(input_layer)
         second_hidden_layer = layers.Dense(48, activation="relu")(first_hidden_layer)
 
         output_layer = layers.Dense(output_size, activation="linear")(second_hidden_layer)
@@ -30,14 +31,26 @@ def buildModel(input_size, output_size):
 
         return model
 
+def changeReward(next_observation, reward, done, info):
+        if done:
+          return - reward
+
+        return reward
 
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v1").env
     model = buildModel(env.observation_space.shape[0],env.action_space.n)
+   
+    deepQlearning  = DeepQLearning(model,env.action_space.sample,gamma=0.99, size_memory=None,epsilon_decay=0.996)
 
-    deepQlearning  = DeepQLearning(model,env.action_space.sample)
+#     deepQlearning.load("model")
 
-    bridge = GymBridge(env,deepQlearning,toInputArray)
 
-    bridge.trainAgent(debug=True, numb_of_matches=5000)
+    bridge = GymBridge(env,deepQlearning,toInputArray,changeReward,limit_reward=1e3)
+
+    bridge.trainAgent(debug=True, numb_of_matches=1200)
+
+    rewards , timesteps = bridge.testAgent(numb_of_matches=100)
+    
+    print("average reward",rewards.mean())
